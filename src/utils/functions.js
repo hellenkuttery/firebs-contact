d77a42a554942aef30faaa5031e60620f7db3a7a
hellenkuttery <helen@clarusway.com>
2021-12-22 22:46:23 +0300

    add functions

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,4 +1,5 @@
-import { getDatabase, ref, update ,child,push} from "firebase/database";
+import { getDatabase, ref, update ,child,push,onValue} from "firebase/database";
+import { useEffect, useState } from 'react';
 
 export const addInfo=(info)=> {
   const db = getDatabase();
@@ -12,3 +13,26 @@ export const addInfo=(info)=> {
   updates['contact' + newUserKey] = addUser;
   return update(ref(db), updates);
   }
+
+  // export const useFetch = () => {
+  //   const [contactList, setContactList] = useState();
+  //   const [isLoading, setIsLoading] = useState(true);
+  
+  //   useEffect(() => {
+  //     setIsLoading(true);
+  //     const db = getDatabase();
+  //     const userCountRef=ref(db,"contact");
+  //     onValue(userCountRef,(snapshot) => {
+  //       const contacts = snapshot.val();
+  //       console.log({ contacts });
+  //       const contactArray = [];
+  //       for (let id in contacts) {
+  //         contactArray.push({ id, ...contacts[id] });
+  //       }
+  //       setContactList(contactArray);
+  //       setIsLoading(false);
+  //     });
+  //   }, []);
+  
+  //   return { contactList, isLoading };
+  // };

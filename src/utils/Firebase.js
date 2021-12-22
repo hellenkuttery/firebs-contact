8e0048cf2f5bdb74c5a0f099c27720e1af5dadfc
hellenkuttery <helen@clarusway.com>
2021-12-22 18:17:56 +0300

    add database

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -1,14 +1,16 @@
-import * as firebase from "firebase/app";
-import "firebase/database";
+import { initializeApp } from "firebase/app";
+// import {getDatabase} from "firebase/database";
+
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBlPigizlLavoZy_J7LRV6d1ZxGP1fwacs",
-    authDomain: "fire-contact-38dab.firebaseapp.com",
-    projectId: "fire-contact-38dab",
-    storageBucket: "fire-contact-38dab.appspot.com",
-    messagingSenderId: "999909912843",
-    appId: "1:999909912843:web:08bae176db0885bec3720e"
-  };
-firebase.initializeApp(firebaseConfig)
-console.log("firebase",firebase)
+  apiKey: "AIzaSyBlPigizlLavoZy_J7LRV6d1ZxGP1fwacs",
+  authDomain: "fire-contact-38dab.firebaseapp.com",
+  databaseURL: "https://fire-contact-38dab-default-rtdb.firebaseio.com",
+  projectId: "fire-contact-38dab",
+  storageBucket: "fire-contact-38dab.appspot.com",
+  messagingSenderId: "999909912843",
+  appId: "1:999909912843:web:08bae176db0885bec3720e"
+};
+  const firebase= initializeApp(firebaseConfig)
+
 export default firebase;
--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,7 +1,20 @@
-import firebase from './firebase';
+import firebase from "./firebase"
+import { getDatabase, ref, set } from "firebase/database";
 
-export const addInfo=(info)=>{
+
+export const addInfo=(info)=> {
     console.log(info)
-    const contactRef=firebase.database().ref();
-    console.log("contact:",contactRef)
-}
+    const db = getDatabase(firebase);
+    set(ref(db, 'contact/'), {
+        username: info.username,
+        phoneNumber:info.phoneNumber ,
+        gender:info.gender });
+
+  }
+
+//   const dbRef = ref(db, `users/{userUid}`)
+// update(dbRef, {displayName: "Firebase9_IsCool"}).then(() => {
+//   console.log("Data updated");
+// }).catch((e) => {
+//   console.log(e);
+// })

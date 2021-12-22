223a5fcb855055e4b25c83a2f9a20ac19d84f467
hellenkuttery <helen@clarusway.com>
2021-12-23 01:42:53 +0300

    add contact list

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,4 +1,4 @@
-import { Table} from 'semantic-ui-react';
+import { Table,Icon} from 'semantic-ui-react';
 import { useFetch } from '../../utils/functions';
 
 const Contacts = () => {
@@ -23,9 +23,44 @@ const Contacts = () => {
               <p>Loading...</p>
             </Table.Cell>
           </Table.Row> ) :
-          (<Table.Row>
-            <Table.Cell>merhaba</Table.Cell>
-          </Table.Row>)
+          contactList?.length === 0 ? (
+            <Table.Row>
+              <Table.Cell colSpan={5} textAlign="center">
+                <p className="nothing-found">Nothing Found</p>
+              </Table.Cell>
+            </Table.Row>
+          ) :(
+            contactList?.map((item, index) => (
+              <Table.Row key={index}>
+                <Table.Cell textAlign="center">
+                  {item.username.toUpperCase()}
+                </Table.Cell>
+                <Table.Cell textAlign="center">{item.phoneNumber}</Table.Cell>
+                <Table.Cell textAlign="center">{item.gender}</Table.Cell>
+                <Table.Cell
+                  textAlign="center"
+                  className="delete"
+                  // onClick={() => deleteHandler(item.id)}
+                >
+                  <Icon name="delete" />
+                </Table.Cell>
+                <Table.Cell
+                  textAlign="center"
+                  className="edit"
+                  // onClick={() =>
+                  //   editHandler(
+                  //     item.id,
+                  //     item.username,
+                  //     item.phoneNumber,
+                  //     item.gender
+                  //   )
+                  // }
+                >
+                  <Icon name="edit" />
+                </Table.Cell>
+              </Table.Row>
+            ))
+          )
           }
         </Table.Body>
      
--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,5 +1,5 @@
 import firebase from "./firebase"
-import { getDatabase, ref, push,set} from "firebase/database";
+import { getDatabase, ref, push,set,get, query, onValue} from "firebase/database";
 import { useState,useEffect } from "react";
 
 export const addInfo=(info)=> {
@@ -15,11 +15,26 @@ export const addInfo=(info)=> {
   }
 
   export const useFetch=()=>{
+    const [contactList, setContactList] = useState();
     const [isLoading,setIsLoading]=useState(false)
     useEffect(()=>{
       setIsLoading(true)
+      const db = getDatabase();
+      const userRef = ref(db, 'contact');
+      const usersSnapshot = get(query(userRef))
+      onValue(query(userRef), snapshot => {
+        const contacts=snapshot.val()
+        console.log(snapshot.val())
+        const contactArray = [];
+        for (let id in contacts) {
+          contactArray.push({ id, ...contacts[id] });
+        }
+        setContactList(contactArray);
+        setIsLoading(false);
+        console.log("useEffectin sonuna geldik")
+      })
     },[]);
-    return {isLoading};
+    return {isLoading,contactList};
   }
 
 
